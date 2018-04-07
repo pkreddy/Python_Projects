7fb9dd1e760c4dca478dd1ac5e59cbdec65fedbc
Pranay Katta <pranay_reddy@outlook.com>
2018-04-07 12:40:42 -0400

    updated singly linkedlist

--- DataStructures/basiclinkedlist.py
+++ DataStructures/basiclinkedlist.py
@@ -3,13 +3,79 @@ class Node:
         self.val = val
         self.next = None
 
-    def traverse(self):
-        node = self #start from head node
-        while node != None:
-            print(node.val) #prints the node value
-            node = node.next #move to the next node
+class LinkedList:
+	def __init__(self):
+		self.head = None
+		self.size = 0
 
+	def deleteFirst(self):
+		self.head = self.head.next
+		self.size = self.size - 1
+	
+	def deleteLast(self):
+		node = self.head
+		while node.next.next != None:
+			node = node.next
+		node.next = None
+		self.size = self.size - 1
+	
+	def deleteAt(self,position):
+		node = self.head
+		if position > self.size:
+			print('cannot be deleted since size is',self.size,'which is less than the given position',position)
+		else:
+			if(position == 1):
+				self.deleteFirst()
+			else:
+				self.size = self.size - 1
+				for i in range(1,position-1,1):
+					node = node.next
+				node.next = node.next.next
+				
+	def addToFirst(self,data):
+		node = Node(data)
+		self.size = self.size + 1
+		if self.head == None:
+			self.head = node
+		else:
+			temp = self.head
+			self.head = node
+			self.head.next = temp
+	
+	def addToLast(self,data):
+		self.size = self.size + 1
+		node = Node(data)
+		if self.head == None:
+			self.head = node
+		else:
+			node_traverse = self.head
+			while node_traverse.next != None:
+				node_traverse = node_traverse.next
+			node_traverse.next = node
+	
+	def insertAt(self,data,position):
+		node = self.head
+		if position > self.size:
+			print("cannot insert in linkedlist since the size is",self.size,"but position to insert at is",position)
+		else:
+			if position == 1:
+				self.addToFirst(data)
+			else:
+				self.size = self.size + 1
+				for i in range(1,position-1,1):
+					node = node.next
+				data_node = Node(data)
+				temp = node.next
+				node.next = data_node
+				data_node.next= temp
+		
+	def traverse(self):
+		node = self.head #start from head node
+		while node != None:
+			print(node.val) #prints the node value
+			node = node.next #move to the next node
 
+'''
 node1 = Node(12)
 node2 = Node(14)
 node3 = Node(16)
@@ -17,3 +83,21 @@ node1.next = node2
 node2.next = node3
 
 node1.traverse()
+'''
+
+l = LinkedList()
+l.addToFirst(1)
+l.addToFirst(2)
+l.addToFirst(3)
+l.addToFirst(4)
+l.addToFirst(5)
+l.addToLast(0)
+l.insertAt(10,5)
+l.deleteFirst()
+l.deleteFirst()
+l.deleteLast()
+l.deleteAt(5)
+print("size is",l.size)
+l.traverse()
+
+#print(l.head.val)
---
+++ DataStructures/doubleLinkedList.py
@@ -0,0 +1,91 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def deleteFirst(self):
+        if self.head == None:
+            pass
+        else:
+            self.head = self.head.next
+            
+    def deleteLast(self):
+        if self.head == None:
+            pass
+        else:
+            node_traverse = self.head
+            while node_traverse != None:
+                tmp = node_traverse
+                node_traverse = node_traverse.next
+            tmp.prev.next = None
+    
+    def addToLast(self,data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            node_traverse = self.head
+            while node_traverse != None:
+                tmp = node_traverse
+                node_traverse = node_traverse.next
+            tmp.next = node
+            node.prev = tmp
+            #print(node_traverse.data)
+                
+	def addToFirst(self,data):
+		node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            node.next = self.head
+            node.next.prev = node
+            self.head = node
+
+	def insertAt(self,data,position):
+		node = Node(data)
+		if self.head == None:
+			self.head = node
+		else:
+			if position == 1:
+				addToFirst(data)
+			else:
+				node_traverse = self.head
+				for i in range(1,position):
+					node_traverse = node_traverse.next
+				print(node_traverse.data)
+	
+    def __str__( self ) :
+        s = ""
+        p = self.head
+        if p != None :		
+            while p.next != None :
+                s += p.data
+                p = p.next
+                s += p.data
+        return s
+
+    def traverse(self):
+        node = self.head
+        while node != None:
+            print(node.data)
+            node = node.next
+    
+l = LinkedList()
+l.addToLast('a')
+l.addToLast('c')
+l.traverse()
+l.addToFirst('d')
+#l.add('c')
+l.traverse()
+print("deleting first element")
+l.deleteFirst()
+print("deleting last element")
+l.deleteLast()
+l.traverse()
+#l.insertAt(1,2)
+#print(l)
