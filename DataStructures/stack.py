ad42093d5c334f44741e0b5d83a99f25f42bda2a
Pranay Katta <pranay_reddy@outlook.com>
2018-04-11 20:23:01 -0400

    stack LIFO and FIFO

---
+++ DataStructures/stack.py
@@ -0,0 +1,19 @@
+class stack:
+	def __init__(self):
+		self.s = []
+	
+	def push(self,data):
+		self.s.append(data)
+	
+	def pop(self):
+		self.s.pop()
+	
+	def print_stack(self):
+		print(self.s)
+	
+s = stack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.pop()
+s.print_stack()
