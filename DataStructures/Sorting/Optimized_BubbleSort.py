75e1971a808e60233974a04183e470f85eda7355
Pranay Katta <pranay_reddy@outlook.com>
2019-01-17 09:51:25 -0500

    Create Optimized_BubbleSort.py

---
+++ DataStructures/Sorting/Optimized_BubbleSort.py
@@ -0,0 +1,16 @@
+def BubbleSort(array):
+    n = len(array)
+    for i in range(n):
+        
+        for j in range(0,n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                Swapped = True
+        
+        if Swapped == False:
+            break
+        
+arr = [12,1,13,11,15,16,2,2,5] 
+
+BubbleSort(arr)
+print(arr)
