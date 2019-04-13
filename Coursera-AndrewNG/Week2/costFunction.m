82e77eb3c0f46358ac494b1353b7a738a24489b8
Pranay Katta <pranay_reddy@outlook.com>
2019-04-13 15:21:11 -0400

    ex - 7

--- Coursera-AndrewNG/Week2/costFunction.m
+++
@@ -1,8 +0,0 @@
-function[jVal, gradient] = costFunction(theta)
-jVal = (theta(1)-5)^2 + (theta(2)-5)^2;
-gradient = zeros(2,1);
-graient(1) = 2 * (theta(1) - 5);
-graient(2) = 2 * (theta(2) - 5);
-end
-
-
